e26adb53c1e7e878104c5d8ab26a044c83dec9a0
nian <liuniangreat@gmail.com>
2019-06-22 16:09:20 +0800

    20190622

--- LeetCode.java
+++ LeetCode.java
@@ -1,30 +1,30 @@
-package LeetCode;
-
 public class LeetCode {
 
-    public static void main(String args[])
-    {
-        Solution S = new Solution();
-        S.twoSum(new int[]{2,7,11,15},9);
-        System.out.println("Hello World");
-    }
-    public static class Solution{
-        public int[] twoSum(int[] nums, int target) {
-            int[] res = new int[2];
-            for(int i =0 ;i<nums.length;i++){
-                for(int j = i+1;j<nums.length;j++){
-                    if(nums[i]+nums[j]==target){
+    private int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for(int i =0 ;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
                         res[0] = i;
                         res[1] = j;
-                    }
                 }
             }
-            //System.out.println("1:"+res[0]+"2:"+res[1]);
-            return res;
         }
-        public int reverse(int x) {
-            return 0;
-
-        }
+        //System.out.println("1:"+res[0]+"2:"+res[1]);
+        return res;
     }
+
+    private int reverse(int x) {
+        return 0;
+
+    }
+
+    public static void main(String[] args)
+    {
+        LeetCode S = new LeetCode();
+        int x = 9;
+        int[] rev = S.twoSum(new int[]{2,7,11,15},x);
+        System.out.println(rev[0]);
+    }
+
 }
