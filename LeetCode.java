5929b41b08400d7fed40f1dc6b1799136367dd48
nian liu <nianliu@qualify.encs.concordia.ca>
2019-09-05 15:16:36 -0400

    1

--- LeetCode.java
+++ LeetCode.java
@@ -194,7 +194,19 @@ public class LeetCode {
         }
         return res.next; 
     }
-    
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = digits.length - 1; i >= 0; --i) {
+            if (digits[i] < 9) {
+                ++digits[i];
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        return res;
+    }
     
     public static void main(String[] args)
     {
