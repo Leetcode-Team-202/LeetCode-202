9521b8e012bc92f3ed6ebd8c75de0e5f787d3d6f
nian liu <nianliu@nians-mbp.wireless.concordia.ca>
2019-11-26 12:28:52 -0500

    1

--- heregreat/LeetCode_NL.java
+++ heregreat/LeetCode_NL.java
@@ -399,6 +399,34 @@ public class LeetCode_NL {
         }
         return -1;
     }
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[2];
+        res[0] = -1;
+        res[1] = -1;
+        int left = 0, right = nums.length;
+        while(left<right){
+            int mid = (left + right)/2;
+            if(nums[mid] < target){
+                left = mid +1;
+            }else right = mid;
+        }
+        if(right == nums.length || nums[right] != target){
+            return res;
+        }
+        res[0] = right;
+        right = nums.length;
+        while(left < right){
+            int mid = (left + right)/2;
+            if(nums[mid] <= target) left = mid + 1;
+            else right = mid;
+        }
+        res[1] = right - 1;
+        return res;
+        
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+    }
     
     public static void main(String[] args)
     {
