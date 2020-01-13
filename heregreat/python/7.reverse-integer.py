8a357883fe8cb350d7c73895af56906defbbc575
nian liu <liuniangreat@gmail.com>
2020-01-13 11:54:07 -0500

    7

--- heregreat/python/7.reverse-integer.py
+++ heregreat/python/7.reverse-integer.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        
+        res, ispos = 0, 1
+        if(x < 0):
+            ispos = -1
+            x *= -1
+        while(x!=0):
+            res = res * 10 + x%10
+            if(res > 2147483647):
+                return 0
+            x = int(x / 10)
+        return res*ispos
 # @lc code=end
 
