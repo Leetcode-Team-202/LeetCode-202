fe0e1aae636caabeffc5d386659debb0d959bd49
nian liu <liuniangreat@gmail.com>
2020-01-29 16:11:19 -0500

    1

--- heregreat/python/50.pow-x-n.py
+++ heregreat/python/50.pow-x-n.py
@@ -6,7 +6,18 @@
 
 # @lc code=start
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
+    def myPow(self, x: float, n: int) -> float:     
+        if(n == 0):
+            return 1
+        elif(n < 0):
+            n = -n
+            x = 1/x
+        half = self.myPow(x, n//2)
+        if(n % 2 == 0):
+            return half * half
+        else:
+            return half * half * x 
         
 # @lc code=end
 
+print(Solution().myPow(2,-10))
