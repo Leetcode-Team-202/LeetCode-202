a07bea1c570db20adec8bfcde501ccaf37e9dc25
nian liu <liuniangreat@gmail.com>
2020-02-02 12:52:19 -0500

    0202

--- heregreat/python/58.length-of-last-word.py
+++ heregreat/python/58.length-of-last-word.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        
+        start = 0
+        res = [0]
+        for i in range(len(s)):
+            if(s[i] == ' '):
+                if(start > 0):
+                    res.append(start)
+                start = 0
+            else:
+                start += 1
+        if(start > 0):
+            res.append(start)
+        return res[-1]
 # @lc code=end
 
