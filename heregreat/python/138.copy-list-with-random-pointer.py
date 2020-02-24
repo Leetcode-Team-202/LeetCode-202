f4e9ece41433537c17456f884ffb60d68b6556c0
nian liu <liuniangreat@gmail.com>
2020-02-24 16:49:33 -0500

    2/24

--- heregreat/python/138.copy-list-with-random-pointer.py
+++ heregreat/python/138.copy-list-with-random-pointer.py
@@ -5,16 +5,26 @@
 #
 
 # @lc code=start
-"""
-# Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
+
+# Definition for a Node.
+
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        
+        copies = {None : None}
+        help = head
+        while help:
+            for node in [help, help.next, help.random]:
+                if node not in copies:
+                    copies[node] = Node(node.val)
+            copies[help].next = copies[help.next]
+            copies[help].random = copies[help.random]
+
+            help = help.next
+        return copies[head]
 # @lc code=end
 
