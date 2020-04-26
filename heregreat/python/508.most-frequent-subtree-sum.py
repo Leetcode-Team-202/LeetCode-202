a1ee71f91531cfac209a7350e35359991c5a43a8
nian liu <liuniangreat@gmail.com>
2020-04-26 14:37:41 -0400

    1

--- heregreat/python/508.most-frequent-subtree-sum.py
+++ heregreat/python/508.most-frequent-subtree-sum.py
@@ -14,6 +14,26 @@
 
 class Solution:
     def findFrequentTreeSum(self, root: TreeNode) -> List[int]:
-        
+        res = []
+        m = {}
+        self.cnt = 0
+        self.preorder(root, res, m)
+        return res
+    def preorder(self, root, res, m):
+        if not root:
+            return 0
+        left = self.preorder(root.left, res, m)
+        right = self.preorder(root.right, res, m)
+        sum = left + right + root.val
+        if(sum not in m.keys()):
+            m[sum] = 0
+        m[sum] += 1
+        if(m[sum] >= self.cnt):           
+            if(m[sum] > self.cnt):
+                res.clear()
+            res.append(sum)
+            self.cnt = m[sum]
+        return sum 
+
 # @lc code=end
 
