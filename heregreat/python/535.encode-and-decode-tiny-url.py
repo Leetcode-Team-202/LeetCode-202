7122db6b359139c842a80ff401873dbdc053b156
nian liu <liuniangreat@gmail.com>
2020-05-13 15:18:56 -0400

    new

--- heregreat/python/535.encode-and-decode-tiny-url.py
+++ heregreat/python/535.encode-and-decode-tiny-url.py
@@ -3,22 +3,39 @@
 #
 # [535] Encode and Decode TinyURL
 #
-
+from random import randint, sample
 # @lc code=start
 class Codec:
+    def __init__(self):
+        self.short_dict = {}
+        self.long_dict = {}
 
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        
+        if longUrl not in self.long_dict.keys():
+            word = self.generate_word()
+            while(word in self.short_dict.keys()):
+                word = self.generate_word()
+            self.long_dict[longUrl] = word
+            self.short_dict[word] = longUrl
+
+        return self.long_dict[longUrl]
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        
+        return self.short_dict[shortUrl]
+
+    def generate_word(self):
+        "generate unique 6 bit code from dic"
+        dic = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        word = sample(dic,6)
+        return 'http://tinyurl.com/' + str(word)
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.decode(codec.encode(url))
 # @lc code=end
-
+Codec().encode("https://leetcode.com/problems/design-tinyurl")
+print(Codec().decode("http://tinyurl.com/4e9iAk"))
---
+++ heregreat/python/537.complex-number-multiplication.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=537 lang=python3
+#
+# [537] Complex Number Multiplication
+#
+
+# @lc code=start
+class Solution:
+    def complexNumberMultiply(self, a: str, b: str) -> str:
+        
+# @lc code=end
+
